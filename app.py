4ab391b7614aade9cb3d10680bbfb68ceb02fc2d
MBLE9 <mirandaluis41@gmail.com>
2025-08-26 22:10:42 -0600

    initial commit

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
 # Este archivo dictara la lógica de la aplicación python
+
+import streamlit as st
+
+st.header('Lanzar una moneda')
+
+st.write('Esta aplicación aún no es funcional. En construcción.')
+
+
